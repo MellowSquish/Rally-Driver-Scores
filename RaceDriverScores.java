f31d34b46d049ff531f473d23359abaa83de1abe
Chloe Capriotti <c.s.capriotti@gmail.com>
2025-05-15 13:20:07 -0400

    Add files via upload

---
+++ RaceDriverScores.java
@@ -0,0 +1,40 @@
+/**
+*Program: CET Level 2
+*Student Name: ______________________
+*Student Number:_______________
+*Course: CST8132_310 OOP   Section: ________
+*Professor: James Mwangi PhD.
+*/
+
+
+
+public class RaceDriverScores {
+
+	public static void main(String[] args) {
+		//TODO declare two-dimensional array here:
+		
+		
+		
+		//TODO declare one-dimensional array here:
+		
+		
+
+		//TODO Calculate total scores for each driver here:
+
+		
+
+		//TODO Calculate total scores for each race here:
+
+		
+
+		//TODO Print total scores for each driver:
+
+		
+
+		//TODO Print total scores for each race:
+		
+		
+
+	}
+
+}
